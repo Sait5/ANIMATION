5f1316382fb26218a92c0162db87cbf6fffcdaf3
Sait5 <balaevs134@gmail.com>
2024-12-21 23:22:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('burgerMenu').addEventListener('click', function () {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active'); // Toggles the active class to show/hide the menu
+});
